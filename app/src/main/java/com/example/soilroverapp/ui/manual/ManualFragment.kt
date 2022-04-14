f8391f9c4c0798a266fcbd73d99dc865878d71aa
raily2595 <raily2595@hotmail.de>
2022-04-14 13:49:39 +0200

    added possibilty for showing manual

--- app/src/main/java/com/example/soilroverapp/ui/manual/ManualFragment.kt
+++ app/src/main/java/com/example/soilroverapp/ui/manual/ManualFragment.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
+import com.example.soilroverapp.R
 import com.example.soilroverapp.databinding.FragmentManualBinding
+import com.github.barteksc.pdfviewer.PDFView
+
 
 class ManualFragment : Fragment() {
 
+    var pdfView: PDFView? = null
     private var _binding: FragmentManualBinding? = null
 
     // This property is only valid between onCreateView and
@@ -27,12 +30,12 @@ class ManualFragment : Fragment() {
 
         _binding = FragmentManualBinding.inflate(inflater, container, false)
         val root: View = binding.root
-
-        val textView: TextView = binding.textManual
-        manualViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
         return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        pdfView = requireView().findViewById(R.id.pdfView1) as PDFView
+        pdfView!!.fromAsset("PM utstilling 2022.pdf").load()
     }
 
     override fun onDestroyView() {
